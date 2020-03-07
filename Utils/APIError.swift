21594308ce790f5434db8e8f8319d3b9e74fcd53
Andreas Grahn <andreas.grahn@knowit.no>
2020-03-07 10:02:14 +0100

    added new Error enum

---
+++ Utils/APIError.swift
@@ -0,0 +1,14 @@
+//
+//  APIError.swift
+//  ArtistBrowser
+//
+//  Created by Andreas Grahn on 07/03/2020.
+//  Copyright © 2020 Andreas Grahn. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case network(_ statusCode: Int)
+    case unknown(_ error: Error)
+}
