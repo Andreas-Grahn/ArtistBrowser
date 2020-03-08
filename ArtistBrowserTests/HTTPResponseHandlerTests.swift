3d7a2b89cfdaa02c9ba4e768da3ba61aa55794f5
andreas-grahn <andreas.grahn.1995@gmail.com>
2020-03-08 16:36:08 +0100

    added four unittests for the HTTP ResponseHandler

---
+++ ArtistBrowserTests/HTTPResponseHandlerTests.swift
@@ -0,0 +1,77 @@
+//
+//  HTTPResponseHandlerTests.swift
+//  ArtistBrowserTests
+//
+//  Created by Andreas Grahn on 08/03/2020.
+//  Copyright © 2020 Andreas Grahn. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import ArtistBrowser
+
+final class HTTPResponseHandlerTests: XCTestCase {
+    private let responseHandler = ResponseHandler()
+    func testHandle_withNilResponseAndNilError_shouldThrowUnknownType() {
+        do {
+            try responseHandler.handle(data: nil, response: nil, error: nil)
+            XCTFail()
+        } catch let error as APIError {
+            guard case APIError.unknownResponseType = error else {
+                XCTFail()
+                return
+            }
+            XCTAssert(true)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testHandle_WithInteralServerError_shouldThrowUnknownType() {
+        let url = URL(string: "https://api.deezer.com/")!
+        let response = HTTPURLResponse(
+            url: url,
+            statusCode: 500,
+            httpVersion: nil,
+            headerFields: nil
+        )
+
+        do {
+            try responseHandler.handle(data: nil, response: response, error: nil)
+            XCTFail()
+        } catch let error as APIError {
+            guard case APIError.unknownResponseType = error else {
+                XCTFail()
+                return
+            }
+            XCTAssert(true)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testHandle_withNilResponse_shouldThrowDeviceError() {
+        let error = NSError(domain: "TESTERROR", code: -1, userInfo: nil)
+        do {
+            try responseHandler.handle(data: nil, response: nil, error: error)
+        } catch let error as APIError {
+            guard case let APIError.device(clientError) = error else {
+                XCTFail()
+                return
+            }
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testHandle_withOKResponse_shouldNotThrow() {
+        let url = URL(string: "https://api.deezer.com/")!
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        do {
+            try responseHandler.handle(data: nil, response: response, error: nil)
+            XCTAssert(true)
+        } catch {
+            XCTFail()
+        }
+    }
+}
